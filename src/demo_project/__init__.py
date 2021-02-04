a6dcc0710d4c9986d5c5ea1eb6ec055581cc0490
Carl Schreep <carl.schreep@fastmail.com>
2021-02-04 09:37:37 -0500

    Adding docstrings and doctest

--- src/demo_project/__init__.py
+++ src/demo_project/__init__.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 
 
 def count(query_string: str) -> Dict[str, int]:
+    """
+    Takes a string and returns a dictionary with the counts of
+    each individual character.
+
+    >>> count('hi')
+    {'h': 1, 'i': 1}
+
+    >>> count('')
+    {}
+
+    >>> count("!")
+    {'!': 1}
+    """
+
     counts = defaultdict(lambda: 0)
 
     for char in query_string:
@@ -17,6 +31,12 @@ def count(query_string: str) -> Dict[str, int]:
 
 @app.route('/')
 def hello_world():
+    """
+    This endpoint takes a single querystring parameter, "query",
+    And returns a JSON object with the counts for each individual character in the parameter
+
+    """
+
     count_query = request.args.get('query')
 
     if count_query is None:
